13fdad6424bb08037b8f0e0767c8803acc27ff5b
Johannes Heilmann <johannes.heilmann@tum.de>
2020-11-28 16:09:17 +0100

    Added project-service, -repository and model (exercise 05).

--- backend/project/src/main/java/edu/tum/ase/project/ProjectApplication.java
+++ backend/project/src/main/java/edu/tum/ase/project/ProjectApplication.java
@@ -1,5 +1,7 @@
 package edu.tum.ase.project;
 
+import edu.tum.ase.project.model.Project;
+import edu.tum.ase.project.service.ProjectService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +10,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @SpringBootApplication
 public class ProjectApplication implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(ProjectApplication.class);
+
     @Autowired
     DataSource dataSource;
+
+    @Autowired
+    private ProjectService projectService;
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
@@ -23,5 +30,11 @@ public class ProjectApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         log.info("DataSource = " + dataSource);
+        Project project = projectService.createProject(new Project("my-project"));
+        log.info("ID of saved project = " + project.getId());
+        Project p = projectService.findByName("my-project");
+        log.info("ID of queried project = " + p.getId());
+        List<Project> projects = projectService.getProjects();
+        log.info("Length of project list = " + projects.size());
     }
 }
---
+++ backend/project/src/main/java/edu/tum/ase/project/model/Project.java
@@ -0,0 +1,45 @@
+package edu.tum.ase.project.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(name = "project_id")
+    private String id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    // ... additional members, often include @OneToMany mappings
+
+    protected Project() {
+        // no-args constructor required by JPA spec
+        // this one is protected since it shouldn't be used directly
+    }
+
+    public Project(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
