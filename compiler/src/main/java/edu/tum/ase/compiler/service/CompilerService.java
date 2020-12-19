9213d22af349239f6ca67cdae71889feba30728b
hohmannr <raphael.hohmann@tuta.io>
2020-12-19 20:29:15 +0100

    added C Code compilation to compiler microservice

--- compiler/src/main/java/edu/tum/ase/compiler/service/CompilerService.java
+++ compiler/src/main/java/edu/tum/ase/compiler/service/CompilerService.java
@@ -5,14 +5,18 @@ import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.stereotype.Service;
 
 import javax.tools.*;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
+import javax.xml.transform.Source;
+import java.io.*;
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 @Service
 public class CompilerService {
+    // C compilation constants
+    String tmpDir = "./tmp";
+    String inDir = tmpDir + "/in";
+    String outDir = tmpDir + "/out";
+
     public SourceCode compile(SourceCode sourceCode) throws IOException {
         return switch (sourceCode.getFileEnding()) {
             case "java" -> compileJavaCode(sourceCode);
@@ -50,17 +54,68 @@ public class CompilerService {
         }
 
         fileManager.close();
-        cleanUpFiles(sourceFile);
+        cleanUpSourceFile(sourceFile);
 
         return sourceCode;
     }
 
     private SourceCode compileCCode(SourceCode sourceCode) throws IOException {
+        // create source file and input and output dirs
+        File sourceFile = createSourceFile(sourceCode);
+        createInOutDirs();
+
+        // get runtime process for compilation with GCC
+        String cmd = buildGCCCommand(sourceFile);
+        Process proc = Runtime.getRuntime().exec(cmd);
+
+        // define writers that listen to proc's i/o
+        BufferedReader stderrReader = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+
+        // try catch, could be that proc gets interrupted
+        try {
+            int exitCode = proc.waitFor();
+
+            // check if compilation was successful
+            if (exitCode == 0) {
+                sourceCode.setCompilable(true);
+            } else {
+                sourceCode.setCompilable(false);
+
+                String stderr = stderrReader.lines().collect(Collectors.joining("\n"));
+                sourceCode.setStderr(stderr);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        stderrReader.close();
+        cleanUpSourceFile(sourceFile);
+
         return sourceCode;
     }
 
+    private String buildGCCCommand(File sourceFile) {
+        String outputPath = outDir + "/" + sourceFile.getName().substring(0, sourceFile.getName().length() - 2);
+        System.out.println("out: " + outputPath);
+        String cmd = String.format("gcc %s -o %s", sourceFile.getAbsolutePath(), outputPath);
+
+        return cmd;
+    }
+
+    private void createInOutDirs() {
+        File inputDir = new File(inDir);
+        File outputDir = new File(outDir);
+
+        if (!inputDir.exists()) {
+            inputDir.mkdirs();
+        }
+        if (!outputDir.exists()) {
+            outputDir.mkdirs();
+        }
+    }
+
     private File createSourceFile(SourceCode sourceCode) throws IOException {
-        File sourceFile = new File("tmp/%s".formatted(sourceCode.getFileName()));
+        File sourceFile = new File("%s/%s".formatted(inDir, sourceCode.getFileName()));
 
         // create temporary folder to store source file and compilation results
         if (!sourceFile.getParentFile().exists()) {
@@ -75,7 +130,7 @@ public class CompilerService {
         return sourceFile;
     }
 
-    private void cleanUpFiles(File sourceFile) throws IOException {
+    private void cleanUpSourceFile(File sourceFile) throws IOException {
         FileUtils.deleteDirectory(sourceFile.getParentFile());
     }
 }
