35735f9641a2f240abb43f94f809df734a640548
Raphael Hohmann <raphael.hohmann@tuta.io>
2021-02-01 19:45:51 +0100

    Added docker integration for frontend service

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-build_service() {
+build_backend_service() {
     local service_dir=$1
     local service_name=$2
     cd "$WORKDIR/$service_dir"
@@ -14,14 +14,28 @@ build_service() {
     printf "\n"
 }
 
+build_frontend_service() {
+    local service_dir='ui/'
+    local service_name='frontend-service'
+
+    cd $service_dir
+    echo "Building: $service_name at $WORKDIR/$service_dir"
+    echo -e "\t> docker build"
+    docker build --pull -t $service_name . 1> $BUILD_LOG || { echo "error: Could not build docker image. Check log at $BUILD_LOG"; exit 1; }
+    echo -e "Finished building docker image with name: $service_name"
+    printf "\n"
+}
+
 check_openjdk_version() {
     local jdk_version=$(java --version | head -n 1 | awk '{print $2}')
     [[ $jdk_version != $OPENJDK_VERSION* ]] && echo -e "error: Need at least openjdk-version $OPENJDK_VERSION.\nVersion installed: $jdk_version" && exit 1
 }
 
 WORKDIR=$(pwd)
-SERVICE_DIRS=( 'compiler/' 'dark-mode/' 'project/' 'ui/' )
-SERVICE_NAMES=( 'compiler-service' 'darkmode-service' 'project-service' 'frontend-service' )
+
+# Backend services
+SERVICE_DIRS=( 'compiler/' 'dark-mode/' 'project/' )
+SERVICE_NAMES=( 'compiler-service' 'darkmode-service' 'project-service')
 OPENJDK_VERSION=15
 BUILD_LOG="$WORKDIR/.docker-build.log"
 
@@ -32,8 +46,11 @@ echo -e "Building docker images locally. This process can take several minutes..
 for i in ${!SERVICE_DIRS[@]}; do
     service_dir=${SERVICE_DIRS[$i]}
     service_name=${SERVICE_NAMES[$i]}
-    build_service $service_dir $service_name
+    build_backend_service $service_dir $service_name
 done
+
+# build frontend
+build_frontend_service
 
 echo "Removing logfile. Finished."
 rm $BUILD_LOG
