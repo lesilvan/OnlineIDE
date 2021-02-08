6054f2278523ed7c383bc2f76c743cd35200a60a
Johannes Heilmann <johannes.heilmann@tum.de>
2021-02-08 11:25:46 +0100

    Cleaned up commented put code.

--- gateway/src/main/java/edu/tum/ase/gateway/GatewayApplication.java
+++ gateway/src/main/java/edu/tum/ase/gateway/GatewayApplication.java
@@ -2,21 +2,12 @@ package edu.tum.ase.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.context.SecurityContext;
-//import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.security.Principal;
 
 @SpringBootApplication
 @EnableEurekaClient
--- gateway/src/main/java/edu/tum/ase/gateway/WebSecurityConfiguration.java
+++ gateway/src/main/java/edu/tum/ase/gateway/WebSecurityConfiguration.java
@@ -1,34 +1,15 @@
 package edu.tum.ase.gateway;
 
-import com.google.common.collect.Lists;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.security.web.server.csrf.CsrfToken;
-import org.springframework.security.web.session.SessionManagementFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Collections;
+
 
 
 @Configuration
@@ -39,10 +20,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //.csrf().disable()
-                //.cors()
-
-                //.and()
                     .logout()
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                         .invalidateHttpSession(true)
@@ -54,42 +31,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
                 .and()
                     .csrf()
-                    //.csrfTokenRepository(csrfTokenRepository())
-                    //.requireCsrfProtectionMatcher(csrfProtectionMatcher(patterns))
                     .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 
                 .and()
                     .authorizeRequests()
                     .antMatchers("/authenticated").permitAll()
                     .antMatchers("/api/**").authenticated();
-
-                //.and()
-                //    .addFilterAfter(new CsrfGrantingFilter() , SessionManagementFilter.class);
     }
-
-    private CsrfTokenRepository csrfTokenRepository() {
-        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-        repository.setHeaderName("X-XSRF-TOKEN");
-        return repository;
-    }
-
-
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        final UrlBasedCorsConfigurationSource source = new     UrlBasedCorsConfigurationSource();
-//        final CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOrigin(CorsConfiguration.ALL);
-//        config.setAllowedHeaders(Collections.singletonList(CorsConfiguration.ALL));
-//        config.addAllowedMethod("OPTIONS");
-//        config.addAllowedMethod("HEAD");
-//        config.addAllowedMethod("GET");
-//        config.addAllowedMethod("PUT");
-//        config.addAllowedMethod("POST");
-//        config.addAllowedMethod("DELETE");
-//        config.addAllowedMethod("PATCH");
-//        config.setAllowedHeaders(Lists.newArrayList("X-XSRF-TOKEN", "XSRF-TOKEN", "Content-Type"));
-//        source.registerCorsConfiguration("/**", config);
-//        return source;
-//    }
 }
